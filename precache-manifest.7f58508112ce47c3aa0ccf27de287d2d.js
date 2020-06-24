b61886c952b2ad290647330c18020762e9329145
JMACBigMan <jemclaurin@gmail.com>
2020-06-23 20:42:07 -0400

    Updates

---
+++ precache-manifest.7f58508112ce47c3aa0ccf27de287d2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f7efb9cc1e8eb0ea5a11a84af168217",
+    "url": "/memes/index.html"
+  },
+  {
+    "revision": "67552b9b7909598349e0",
+    "url": "/memes/static/css/main.87fe9e4b.chunk.css"
+  },
+  {
+    "revision": "206dd74fb74395f56eb4",
+    "url": "/memes/static/js/2.5c33c587.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memes/static/js/2.5c33c587.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67552b9b7909598349e0",
+    "url": "/memes/static/js/main.beee399a.chunk.js"
+  },
+  {
+    "revision": "b30f392f2e7f00e787c3",
+    "url": "/memes/static/js/runtime-main.a242ee7a.js"
+  }
+]);
